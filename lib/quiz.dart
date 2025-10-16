0060338d4c5fdb4fcd40fe612433c7efff1133d8
unknown <mohamedamine.koubaa@esprit.tn>
2025-10-16 16:41:53 +0100

    switching between start screen and question screen

---
+++ lib/quiz.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
+import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget{
+
+  const Quiz({super.key});
+
+  @override
+ State <Quiz> createState() {
+    return _QuizState();
+  }
+ }
+
+
+class _QuizState extends State<Quiz>{
+   Widget? activeScreen ;
+  @override
+  void initState() {
+    activeScreen = StartScreen(switchScreen);
+    super.initState();
+  }
+  void switchScreen(){
+    setState(() {
+      activeScreen = QuestionsScreen();
+    });
+  }
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [
+                  Colors.deepPurple.shade300,
+                  Colors.indigo.shade900
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight
+            ),
+          ),
+          child: activeScreen,
+
+        ),
+
+      ),
+    );
+  }
+}
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
-
+  const StartScreen(this.startQuiz ,{super.key});
+final void Function() startQuiz;
   @override
   Widget build(context) {
     return Center(
@@ -25,15 +25,18 @@ class StartScreen extends StatelessWidget {
               ),
             ),
             const SizedBox(height: 50),
-            OutlinedButton(
-                onPressed: (){},
+            OutlinedButton.icon(
+                onPressed: (){
+                  startQuiz();
+                },
               style: OutlinedButton.styleFrom(
                 foregroundColor: Colors.white,
                 side: const BorderSide(color: Colors.white),
                 textStyle: const TextStyle(fontSize: 20
               ),
               ),
-                child: const Text('Start Quiz'),
+                icon: const Icon(Icons.arrow_right_alt),
+                label: const Text('Start Quiz'),
             ),
           ],
         ),
